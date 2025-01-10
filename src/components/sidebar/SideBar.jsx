a476e8ac56d56a0b123e202f19377550ddbb9de8
alexnguyen02 <127801307+alexnguyen02@users.noreply.github.com>
2025-01-09 21:07:21 -0500

    Completed the basic design of the website

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,7 +1,7 @@
 import './SideBar.css'; 
-import { useState } from 'react'; 
+import { useState, useEffect } from 'react'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
-import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons'; 
 
 const SideBar = () => {
   const [isOpen, setIsOpen] = useState(false); 
@@ -10,17 +10,31 @@ const SideBar = () => {
     setIsOpen((prevState) => !prevState); 
   }
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }, [isOpen]);
+
   return (
-    <div className='sidebar'>
-      <button className='menu-button' onClick={toggleMenu}>
-        {isOpen
-          ? <FontAwesomeIcon icon={faXmark} className='menu-icon'/>
-          : <FontAwesomeIcon icon={faBars} className='menu-icon'/>
-        }
-      </button>
-
-      
-
+    <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+      <div className='button-container'>
+        <button className='menu-button' onClick={toggleMenu}>
+          {isOpen
+            ? <FontAwesomeIcon icon={faXmark} className='menu-icon'/>
+            : <FontAwesomeIcon icon={faBars} className='menu-icon'/>
+          }
+        </button>
+      </div>
+      <div className={`sidebar-menu ${isOpen ? 'open' : 'closed'}`}>
+          <a className='home-item'>Home</a>
+          <a>About</a>
+          <a>Experience</a>
+          <a>Projects</a>
+        <a className='contact-item'>Contact</a>
+      </div>
     </div>
   );
 };
